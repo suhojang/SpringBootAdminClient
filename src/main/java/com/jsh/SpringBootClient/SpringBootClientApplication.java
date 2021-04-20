9c2ef3d6224931992b6c7ceebbcfbb4e8d65678d
suhojang <jsh2808@naver.com>
2021-04-20 13:41:22 +0900

    Initial commit

---
+++ src/main/java/com/jsh/SpringBootClient/SpringBootClientApplication.java
@@ -0,0 +1,13 @@
+package com.jsh.SpringBootClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientApplication.class, args);
+	}
+
+}
